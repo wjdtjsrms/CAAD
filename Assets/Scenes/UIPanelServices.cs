c1d1d0998241da4c1606833a8b93da9c1dc3f709
wjdtjsrms <9kjmse@naver.com>
2023-04-20 23:02:16 +0900

    <Update List> Jsg 1. 기본 기능 추가

--- Assets/Scenes/UIPanelServices.cs
+++ Assets/Scenes/UIPanelServices.cs
@@ -3,11 +3,49 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIPanelServices : MonoBehaviour
 {
-    public async void OpenAEDPage()
+    [SerializeField] private Toggle toggleCall;
+    [SerializeField] private Toggle togglePlayCPR;
+    [SerializeField] private Button btnFindNearAED;
+
+    private AudioSource audioCPR;
+
+    void Awake()
+    {
+        toggleCall.onValueChanged.AddListener(OnValuedChangeToggle119);
+        togglePlayCPR.onValueChanged.AddListener(OnValueChangedToggleCPR);
+        btnFindNearAED.onClick.AddListener(OpenAEDPage);
+
+        audioCPR = togglePlayCPR.GetComponent<AudioSource>();
+    }
+
+    private void OnDestroy()
+    {
+        toggleCall.onValueChanged.RemoveAllListeners();
+        togglePlayCPR.onValueChanged.RemoveAllListeners();
+        btnFindNearAED.onClick.RemoveAllListeners();
+    }
+
+    public void OpenAEDPage()
     {
         Application.OpenURL("https://www.e-gen.or.kr/egen/search_aed.do");
+    }
+
+    public void OnValueChangedToggleCPR(bool isOn)
+    {
+        if (isOn)
+            audioCPR.Play();
+        else
+            audioCPR.Stop();
+
+        togglePlayCPR.graphic.gameObject.SetActive(isOn);
+    }
+
+    public void OnValuedChangeToggle119(bool isOn)
+    {
+        toggleCall.graphic.gameObject.SetActive(isOn);
     }
 }
